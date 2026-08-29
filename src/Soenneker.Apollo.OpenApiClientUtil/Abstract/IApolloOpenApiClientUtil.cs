ba244e55b5dcc9e49aa339589e66aebd2171b26d
soenneker <jake@soenneker.com>
2026-08-29 14:16:45 -0500

    Improve public API XML documentation

--- src/Soenneker.Apollo.OpenApiClientUtil/Abstract/IApolloOpenApiClientUtil.cs
+++ src/Soenneker.Apollo.OpenApiClientUtil/Abstract/IApolloOpenApiClientUtil.cs
@@ -2,13 +2,16 @@ using Soenneker.Apollo.OpenApiClient;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-
 namespace Soenneker.Apollo.OpenApiClientUtil.Abstract;
-
 /// <summary>
 /// Exposes a cached OpenAPI client instance.
 /// </summary>
 public interface IApolloOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Returns the configured apollo OpenAPI Client used by the Apollo OpenAPI Client.
+    /// </summary>
+    /// <param name="cancellationToken">Token used to cancel the operation.</param>
+    /// <returns>A task whose result is the requested apollo OpenAPI Client.</returns>
     ValueTask<ApolloOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
--- src/Soenneker.Apollo.OpenApiClientUtil/Registrars/ApolloOpenApiClientUtilRegistrar.cs
+++ src/Soenneker.Apollo.OpenApiClientUtil/Registrars/ApolloOpenApiClientUtilRegistrar.cs
@@ -13,6 +13,8 @@ public static class ApolloOpenApiClientUtilRegistrar
     /// <summary>
     /// Adds <see cref="ApolloOpenApiClientUtil"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddApolloOpenApiClientUtilAsSingleton(this IServiceCollection services)
     {
         services.AddApolloOpenApiHttpClientAsSingleton()
@@ -24,6 +26,8 @@ public static class ApolloOpenApiClientUtilRegistrar
     /// <summary>
     /// Adds <see cref="ApolloOpenApiClientUtil"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddApolloOpenApiClientUtilAsScoped(this IServiceCollection services)
     {
         services.AddApolloOpenApiHttpClientAsSingleton()
